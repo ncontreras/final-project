1290846709e03d0efb928d3304927dd5c600b363
Nancy Contreras <ncontreras@luc.edu>
2017-12-10 20:13:34 -0600

    created word.html page

---
+++ js/word.js
@@ -0,0 +1,8 @@
+var word = ["xenophobic" , "preaccept", "duffer", "servants", "unbecomingly", "commended", "sonic", "jonathan", "advise", "parade", 
+"remarriages", "combos", "superconductor", "trackless", "uprousing", "emancipation", "lasciviousness", "stillest", 
+"conflictive", "scavenges"]
+
+let guess_word = word[Math.floor(Math.random() * word.length)];
+
+console.log(guess_word);
+
